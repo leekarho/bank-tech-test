8e46294cf887c9fd35cde405b72d7156928edecb
leekarho <k-h_lee@hotmail.co.uk>
2018-10-08 11:10:44 +0100

    first commit

---
+++ lib/bank.rb
@@ -0,0 +1,17 @@
+class Bank
+
+  # attr_reader :balance
+  #
+  # def initialize
+  #   @balance = 0
+  # end
+  #
+  # def credit(deposit)
+  #   @balance += deposit
+  # end
+  #
+  # def debit(withdrawal)
+  #   @balance -= withdrawal
+  # end
+
+end
---
+++ spec/bank_spec.rb
@@ -0,0 +1,18 @@
+require 'bank'
+
+describe Bank do
+
+  # it 'increases balance when credit' do
+  #   bank = Bank.new
+  #   bank.credit(1000)
+  #   expect(bank.balance).to eq 1000
+  # end
+  #
+  # it 'decreases balance when debit' do
+  #   bank = Bank.new
+  #   bank.credit(1000)
+  #   bank.debit(250)
+  #   expect(bank.balance).to eq 750
+  # end
+
+end
