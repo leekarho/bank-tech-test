fdf13c6b53130d1abf65072727b34cbbd5e1c867
leekarho <k-h_lee@hotmail.co.uk>
2018-10-08 11:11:37 +0100

    can credit account

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,14 +1,14 @@
 class Bank
 
-  # attr_reader :balance
-  #
-  # def initialize
-  #   @balance = 0
-  # end
-  #
-  # def credit(deposit)
-  #   @balance += deposit
-  # end
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def credit(deposit)
+    @balance += deposit
+  end
   #
   # def debit(withdrawal)
   #   @balance -= withdrawal
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,11 +2,11 @@ require 'bank'
 
 describe Bank do
 
-  # it 'increases balance when credit' do
-  #   bank = Bank.new
-  #   bank.credit(1000)
-  #   expect(bank.balance).to eq 1000
-  # end
+  it 'increases balance when credit' do
+    bank = Bank.new
+    bank.credit(1000)
+    expect(bank.balance).to eq 1000
+  end
   #
   # it 'decreases balance when debit' do
   #   bank = Bank.new
